effca8903c7d61006b48615667350d569da21cc5
ahmed nur <ahmednur103308@gmail.com>
2024-11-09 21:46:21 +0600

    first commit

---
+++ src/comononets/login/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div>
+          <h1 className="text-4xl">LogIn Page</h1>  
+        </div>
+    );
+};
+
+export default Login;
---
+++ src/main.jsx
@@ -0,0 +1,43 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Header from './comononets/header/Header';
+import Main from './leyout/Main';
+import Home from './comononets/home/Home';
+import Login from './comononets/login/Login';
+import Register from './comononets/register/Register';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children:[{
+      path:"/",
+      element:<Home></Home>
+    },
+    {
+      path:"/login",
+      element:<Login></Login>
+    },
+
+    {
+      path:"/register",
+      element:<Register></Register>
+    }
+  
+  
+  ]
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+     <RouterProvider router={router} />
+  </StrictMode>,
+)
